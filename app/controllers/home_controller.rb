3ec55bf467cce1d0d014267f71df438e4b4df121
Jin Dhaliwal <jindhaliwal@gmail.com>
2018-03-28 22:48:29 -0400

    add haml-rails and create home controller

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,12 @@
+class HomeController < ApplicationController
+
+  before_action :authenticate_user!, except: [:index]
+
+  def index
+
+  end
+
+  def dashboard
+    render plain: 'DASHBOARD'
+  end
+end
